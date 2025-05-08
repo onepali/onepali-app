01e70b43fcc111277f467afa5a0620b30bf2335f
Laxman Magarati <laxmanmagrati@gmail.com>
2025-05-08 14:49:47 +0545

    :recycle: Refactor code structure for improved readability and maintainability

--- lib/src/core/constants/app_constants.dart
+++ lib/src/core/constants/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String appName = 'MyApp';
+  static const String appName = 'O Nepali';
   static const String appVersion = 'v1.0.0';
 
   static const String dotEnvFileName = '.env';
