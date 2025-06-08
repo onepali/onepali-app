c3176eadf9b9d9f69e9347f880e8d9ff37fcf1d4
Laxman Magarati <laxmanmagrati@gmail.com>
2025-06-08 21:31:16 +0545

    refactor: Remove placeholder comment in service_test.dart

--- test/core/services/service_test.dart
+++ test/core/services/service_test.dart
@@ -3,6 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('service.dart exports', () {
-    expect(true, isTrue); // Placeholder to ensure file is included in coverage
+    expect(true, isTrue);
   });
 }
