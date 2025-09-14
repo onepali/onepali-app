a8536405386c38a293703a9e93b02b2c5efda6d1
Laxman Magarati <laxmanmagrati@gmail.com>
2025-09-14 12:52:40 +0545

    feat(printables)!: add printables provider and screens for printables functionality

--- lib/src/screen/screen.dart
+++ lib/src/screen/screen.dart
@@ -107,6 +107,10 @@ export 'reward/widget/reward_collection_widget.dart';
 export 'reward/widget/choose_reward_widget.dart';
 export 'reward/widget/reward_preview_widget.dart';
 
+/// [Printables] Screen
+export 'printables/printables_screen.dart';
+export 'printables/printables_card.dart';
+
 //* -------------------------------------- PARENT ZONE SCREEN EXPORTS -------------------------------------- *//
 
 /// [Parent] --> [Dashboard]
