8ba8daeec2a1ec66acd2abb27ec6064a86ede1ea
Laxman Magarati <laxmanmagrati@gmail.com>
2025-09-22 14:43:51 +0545

    refactor: update background colors for improved visibility in lesson content cards

--- lib/src/screen/course/lesson/widget/lesson_content_card.dart
+++ lib/src/screen/course/lesson/widget/lesson_content_card.dart
@@ -161,7 +161,7 @@ class _LessonContentCardState extends State<LessonContentCard>
           width: width,
           height: height,
           decoration: BoxDecoration(
-            color: Colors.grey[100],
+            color: AppColors.kTransparentColor,
             borderRadius: BorderRadius.circular(16),
           ),
           child: Center(
@@ -173,7 +173,7 @@ class _LessonContentCardState extends State<LessonContentCard>
                 Text(
                   'Loading video...',
                   style: AppStyles.text12PxRegular.copyWith(
-                    color: AppColors.kGrey,
+                    color: AppColors.kWhite,
                   ),
                 ),
               ],
@@ -215,7 +215,7 @@ class _LessonContentCardState extends State<LessonContentCard>
               width: width,
               height: height,
               decoration: BoxDecoration(
-                color: Colors.grey[100],
+                color: AppColors.kTransparentColor,
                 borderRadius: BorderRadius.circular(16),
               ),
               child: const Center(
