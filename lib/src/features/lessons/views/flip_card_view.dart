6c6de28d41e7ba12c3a404500381baa2336d3a70
Saroj Basnet <saroj.basnet1237@gmail.com>
2026-03-11 16:25:53 +0545

    feat: Integrate audio player service, enhance UI components, and add goal animation in penalty slider

--- lib/src/features/lessons/views/flip_card_view.dart
+++ lib/src/features/lessons/views/flip_card_view.dart
@@ -4,9 +4,11 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_flip_card/flutter_flip_card.dart';
+import 'package:onepali/src/core/core.dart';
 import 'package:onepali/src/core/utils/color_from_hex.dart';
 import 'package:onepali/src/core/widget/common/back_arrow_button.dart';
 import 'package:onepali/src/core/widget/common/close_button.dart';
+import 'package:onepali/src/core/widget/common/custom_cache_image.dart';
 import 'package:onepali/src/core/widget/common/forward_arrow_button.dart';
 import 'package:onepali/src/features/lessons/blocs/lession_bloc/lesson_bloc.dart';
 import 'package:onepali/src/features/lessons/models/lesson.dart';
@@ -50,70 +52,73 @@ class _FlipCardViewState extends State<FlipCardView> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+    final isMobile = PlatformUtility.isMobile(context);
     return Stack(
       children: [
         Positioned.fill(
-          child: GridView(
-            padding: EdgeInsets.all(size.width * 0.1),
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              mainAxisSpacing: size.width * 0.05,
-              crossAxisSpacing: size.width * 0.05,
-            ),
-            children: widget.content.items
-                .map(
-                  (e) => Column(
-                    children: [
-                      FlipCard(
-                        rotateSide: RotateSide.bottom,
-                        animationDuration: Duration(milliseconds: 500),
-                        onTapFlipping: true,
-                        axis: FlipAxis
-                            .values[_random.nextInt(FlipAxis.values.length)],
-                        controller:
-                            _controllers[widget.content.items.indexOf(e)],
-                        frontWidget: Transform.rotate(
-                          angle: 0.15,
-                          child: Container(
-                            height: 200,
-                            width: 200,
-                            decoration: BoxDecoration(
-                              color: colorFromHex(e.bgColor),
-                              borderRadius: BorderRadius.circular(16),
+          child: Padding(
+            padding: EdgeInsets.all(isMobile ? 8.0 : 0),
+            child: SafeArea(
+              child: GridView(
+                padding: EdgeInsets.all(isMobile ? 24 : size.width * 0.1),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: isMobile
+                      ? size.width * 0.01
+                      : size.width * 0.05,
+                  crossAxisSpacing: isMobile
+                      ? size.width * 0.01
+                      : size.width * 0.05,
+                  childAspectRatio: isMobile ? 1.5 : 1.0,
+                ),
+                children: widget.content.items
+                    .map(
+                      (e) => Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          FlipCard(
+                            rotateSide: RotateSide.bottom,
+                            animationDuration: Duration(milliseconds: 500),
+                            onTapFlipping: true,
+                            axis:
+                                FlipAxis.values[_random.nextInt(
+                                  FlipAxis.values.length,
+                                )],
+                            controller:
+                                _controllers[widget.content.items.indexOf(e)],
+                            frontWidget: Transform.rotate(
+                              angle: 0.15,
+                              child: Container(
+                                height: isMobile ? 100 : 200,
+                                width: isMobile ? 100 : 200,
+                                decoration: BoxDecoration(
+                                  color: colorFromHex(e.bgColor),
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                              ),
+                            ),
+                            backWidget: Container(
+                              height: isMobile ? 100 : 200,
+                              width: isMobile ? 100 : 200,
+                              decoration: BoxDecoration(
+                                color: colorFromHex(e.bgColor),
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                              child: CustomCachedImage(imageUrl: e.image),
                             ),
                           ),
-                        ),
-                        backWidget: Container(
-                          height: 200,
-                          width: 200,
-                          decoration: BoxDecoration(
-                            color: colorFromHex(e.bgColor),
-                            borderRadius: BorderRadius.circular(16),
+                          const SizedBox(height: 12),
+                          Text(
+                            e.nameNp,
+                            style: Theme.of(context).textTheme.headlineLarge
+                                ?.copyWith(fontWeight: FontWeight.bold),
                           ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.sports_soccer,
-                                color: Colors.white,
-                                size: 60,
-                              ),
-                            ],
-                          ),
-                        ),
+                        ],
                       ),
-                      const SizedBox(height: 12),
-                      Text(
-                        e.nameNp,
-                        style: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-                .toList(),
+                    )
+                    .toList(),
+              ),
+            ),
           ),
         ),
         TopRightPositionedCloseButton(
