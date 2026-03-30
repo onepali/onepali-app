2939f1bf59bb7c37801f1b4bfdb6c601652ea05b
Saroj Basnet <saroj.basnet1237@gmail.com>
2026-03-30 16:47:56 +0545

    refactor: Wrap lesson, songs and story tab UI with safe area to avoid content behind device UI elements like the notch, status bar, or camera cutout

--- lib/src/screen/story/story_screen.dart
+++ lib/src/screen/story/story_screen.dart
@@ -37,83 +37,85 @@ class _StoryScreenState extends State<StoryScreen> {
     bool isTabletLandscape =
         PlatformUtility.isTablet(context) &&
         PlatformUtility.isLandscape(context);
-    return StreamBuilder(
-      stream: FirebaseFirestore.instance
-          .collection('story_levels')
-          .orderBy('name', descending: false)
-          .snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          final data = snapshot.data!.docs;
-
-          return ListView.builder(
-            itemCount: data.length,
-            padding: EdgeInsets.symmetric(horizontal: 24),
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) {
-              final data = snapshot.data!.docs;
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    data[index]['name'],
-                    style: AppStyles.text20PxSemiBold.copyWith(
-                      color: AppColors.kBlack,
-                      fontSize: isTabletLandscape ? 24 : 20,
-                      fontWeight: FontWeight.bold,
+    return SafeArea(
+      child: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('story_levels')
+            .orderBy('name', descending: false)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final data = snapshot.data!.docs;
+      
+            return ListView.builder(
+              itemCount: data.length,
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                final data = snapshot.data!.docs;
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      data[index]['name'],
+                      style: AppStyles.text20PxSemiBold.copyWith(
+                        color: AppColors.kBlack,
+                        fontSize: isTabletLandscape ? 24 : 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 16),
-                  SizedBox(
-                    height: isMobile
-                        ? MediaQuery.of(context).size.height * 0.45
-                        : MediaQuery.of(context).size.height * 0.3,
-                    child: StreamBuilder(
-                      stream: _getStoriesStream(data[index]['id']),
-                      builder: (context, snapshot) {
-                        if (snapshot.hasData) {
-                          final data = snapshot.data!.docs;
-                          return Row(
-                            children: [
-                              for (final lesson in data) ...[
-                                ContentCard(
-                                  nameEn: lesson['nameEn'],
-                                  nameNp: lesson['nameNp'],
-                                  image: lesson['thumbnail'],
-                                  bgImage: lesson['bg_image'],
-                                  isImageSvg: true,
-                                  bgColor: lesson['bg_color'],
-                                  onTap: () {
-                                    final story = StoryModel.fromJson(
-                                      lesson.data(),
-                                    );
-                                    Utility.navigateMaterialRoute(
-                                      context,
-                                      StoryContentScreen(
-                                        story: story,
-                                        isFromRecommended: false,
-                                      ),
-                                    );
-                                  },
-                                ),
-                                Gaps.horizontalGapOf(16),
+                    SizedBox(height: 16),
+                    SizedBox(
+                      height: isMobile
+                          ? MediaQuery.of(context).size.height * 0.45
+                          : MediaQuery.of(context).size.height * 0.3,
+                      child: StreamBuilder(
+                        stream: _getStoriesStream(data[index]['id']),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            final data = snapshot.data!.docs;
+                            return Row(
+                              children: [
+                                for (final lesson in data) ...[
+                                  ContentCard(
+                                    nameEn: lesson['nameEn'],
+                                    nameNp: lesson['nameNp'],
+                                    image: lesson['thumbnail'],
+                                    bgImage: lesson['bg_image'],
+                                    isImageSvg: true,
+                                    bgColor: lesson['bg_color'],
+                                    onTap: () {
+                                      final story = StoryModel.fromJson(
+                                        lesson.data(),
+                                      );
+                                      Utility.navigateMaterialRoute(
+                                        context,
+                                        StoryContentScreen(
+                                          story: story,
+                                          isFromRecommended: false,
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                  Gaps.horizontalGapOf(16),
+                                ],
                               ],
-                            ],
-                          );
-                        }
-                        return const CircularProgressIndicator();
-                      },
+                            );
+                          }
+                          return const CircularProgressIndicator();
+                        },
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 16),
-                ],
-              );
-            },
-          );
-        }
-        return SizedBox();
-      },
+                    SizedBox(height: 16),
+                  ],
+                );
+              },
+            );
+          }
+          return SizedBox();
+        },
+      ),
     );
   }
 }
