917998af87bfbd997d886abebcb5880e723b171c
Saroj Basnet <saroj.basnet1237@gmail.com>
2026-04-08 16:03:02 +0545

    update: update gun fill lesson content to drop inside the gun part

--- lib/src/features/lessons/blocs/gun_fill_bloc/gun_fill_bloc.freezed.dart
+++ lib/src/features/lessons/blocs/gun_fill_bloc/gun_fill_bloc.freezed.dart
@@ -752,7 +752,11 @@ as bool,
 /// @nodoc
 mixin _$GunPart {
 
- String get id; String get path; String? get color; bool get isFilled; Item? get item;
+/// Id of the part, also used as the color code, for example #ff0000.
+ String get id;/// Path of the part in svg image
+ String get path;/// Fill color of the part in svg image.
+ String? get color;/// Tracks whether the part has been filled.
+ bool get isFilled; Item? get item;
 /// Create a copy of GunPart
 /// with the given fields replaced by the non-null parameter values.
 @JsonKey(includeFromJson: false, includeToJson: false)
@@ -963,9 +967,13 @@ class _GunPart implements GunPart {
   const _GunPart({required this.id, required this.path, this.color, this.isFilled = false, this.item});
   
 
+/// Id of the part, also used as the color code, for example #ff0000.
 @override final  String id;
+/// Path of the part in svg image
 @override final  String path;
+/// Fill color of the part in svg image.
 @override final  String? color;
+/// Tracks whether the part has been filled.
 @override@JsonKey() final  bool isFilled;
 @override final  Item? item;
 
@@ -1045,7 +1053,10 @@ $ItemCopyWith<$Res>? get item {
 /// @nodoc
 mixin _$GunLabel {
 
- String get path; String? get color;
+/// Path of the label in svg image
+ String get path;/// Fill color of the label in svg image.
+ String? get color;/// Gun part id/color code that this label accepts when dropped on.
+ String? get gunPartId;
 /// Create a copy of GunLabel
 /// with the given fields replaced by the non-null parameter values.
 @JsonKey(includeFromJson: false, includeToJson: false)
@@ -1056,16 +1067,16 @@ $GunLabelCopyWith<GunLabel> get copyWith => _$GunLabelCopyWithImpl<GunLabel>(thi
 
 @override
 bool operator ==(Object other) {
-  return identical(this, other) || (other.runtimeType == runtimeType&&other is GunLabel&&(identical(other.path, path) || other.path == path)&&(identical(other.color, color) || other.color == color));
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is GunLabel&&(identical(other.path, path) || other.path == path)&&(identical(other.color, color) || other.color == color)&&(identical(other.gunPartId, gunPartId) || other.gunPartId == gunPartId));
 }
 
 
 @override
-int get hashCode => Object.hash(runtimeType,path,color);
+int get hashCode => Object.hash(runtimeType,path,color,gunPartId);
 
 @override
 String toString() {
-  return 'GunLabel(path: $path, color: $color)';
+  return 'GunLabel(path: $path, color: $color, gunPartId: $gunPartId)';
 }
 
 
@@ -1076,7 +1087,7 @@ abstract mixin class $GunLabelCopyWith<$Res>  {
   factory $GunLabelCopyWith(GunLabel value, $Res Function(GunLabel) _then) = _$GunLabelCopyWithImpl;
 @useResult
 $Res call({
- String path, String? color
+ String path, String? color, String? gunPartId
 });
 
 
@@ -1093,10 +1104,11 @@ class _$GunLabelCopyWithImpl<$Res>
 
 /// Create a copy of GunLabel
 /// with the given fields replaced by the non-null parameter values.
-@pragma('vm:prefer-inline') @override $Res call({Object? path = null,Object? color = freezed,}) {
+@pragma('vm:prefer-inline') @override $Res call({Object? path = null,Object? color = freezed,Object? gunPartId = freezed,}) {
   return _then(_self.copyWith(
 path: null == path ? _self.path : path // ignore: cast_nullable_to_non_nullable
 as String,color: freezed == color ? _self.color : color // ignore: cast_nullable_to_non_nullable
+as String?,gunPartId: freezed == gunPartId ? _self.gunPartId : gunPartId // ignore: cast_nullable_to_non_nullable
 as String?,
   ));
 }
@@ -1182,10 +1194,10 @@ return $default(_that);case _:
 /// }
 /// ```
 
-@optionalTypeArgs TResult maybeWhen<TResult extends Object?>(TResult Function( String path,  String? color)?  $default,{required TResult orElse(),}) {final _that = this;
+@optionalTypeArgs TResult maybeWhen<TResult extends Object?>(TResult Function( String path,  String? color,  String? gunPartId)?  $default,{required TResult orElse(),}) {final _that = this;
 switch (_that) {
 case _GunLabel() when $default != null:
-return $default(_that.path,_that.color);case _:
+return $default(_that.path,_that.color,_that.gunPartId);case _:
   return orElse();
 
 }
@@ -1203,10 +1215,10 @@ return $default(_that.path,_that.color);case _:
 /// }
 /// ```
 
-@optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( String path,  String? color)  $default,) {final _that = this;
+@optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( String path,  String? color,  String? gunPartId)  $default,) {final _that = this;
 switch (_that) {
 case _GunLabel():
-return $default(_that.path,_that.color);case _:
+return $default(_that.path,_that.color,_that.gunPartId);case _:
   throw StateError('Unexpected subclass');
 
 }
@@ -1223,10 +1235,10 @@ return $default(_that.path,_that.color);case _:
 /// }
 /// ```
 
-@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>(TResult? Function( String path,  String? color)?  $default,) {final _that = this;
+@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>(TResult? Function( String path,  String? color,  String? gunPartId)?  $default,) {final _that = this;
 switch (_that) {
 case _GunLabel() when $default != null:
-return $default(_that.path,_that.color);case _:
+return $default(_that.path,_that.color,_that.gunPartId);case _:
   return null;
 
 }
@@ -1238,11 +1250,15 @@ return $default(_that.path,_that.color);case _:
 
 
 class _GunLabel implements GunLabel {
-  const _GunLabel({required this.path, this.color});
+  const _GunLabel({required this.path, this.color, this.gunPartId});
   
 
+/// Path of the label in svg image
 @override final  String path;
+/// Fill color of the label in svg image.
 @override final  String? color;
+/// Gun part id/color code that this label accepts when dropped on.
+@override final  String? gunPartId;
 
 /// Create a copy of GunLabel
 /// with the given fields replaced by the non-null parameter values.
@@ -1254,16 +1270,16 @@ _$GunLabelCopyWith<_GunLabel> get copyWith => __$GunLabelCopyWithImpl<_GunLabel>
 
 @override
 bool operator ==(Object other) {
-  return identical(this, other) || (other.runtimeType == runtimeType&&other is _GunLabel&&(identical(other.path, path) || other.path == path)&&(identical(other.color, color) || other.color == color));
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _GunLabel&&(identical(other.path, path) || other.path == path)&&(identical(other.color, color) || other.color == color)&&(identical(other.gunPartId, gunPartId) || other.gunPartId == gunPartId));
 }
 
 
 @override
-int get hashCode => Object.hash(runtimeType,path,color);
+int get hashCode => Object.hash(runtimeType,path,color,gunPartId);
 
 @override
 String toString() {
-  return 'GunLabel(path: $path, color: $color)';
+  return 'GunLabel(path: $path, color: $color, gunPartId: $gunPartId)';
 }
 
 
@@ -1274,7 +1290,7 @@ abstract mixin class _$GunLabelCopyWith<$Res> implements $GunLabelCopyWith<$Res>
   factory _$GunLabelCopyWith(_GunLabel value, $Res Function(_GunLabel) _then) = __$GunLabelCopyWithImpl;
 @override @useResult
 $Res call({
- String path, String? color
+ String path, String? color, String? gunPartId
 });
 
 
@@ -1291,10 +1307,11 @@ class __$GunLabelCopyWithImpl<$Res>
 
 /// Create a copy of GunLabel
 /// with the given fields replaced by the non-null parameter values.
-@override @pragma('vm:prefer-inline') $Res call({Object? path = null,Object? color = freezed,}) {
+@override @pragma('vm:prefer-inline') $Res call({Object? path = null,Object? color = freezed,Object? gunPartId = freezed,}) {
   return _then(_GunLabel(
 path: null == path ? _self.path : path // ignore: cast_nullable_to_non_nullable
 as String,color: freezed == color ? _self.color : color // ignore: cast_nullable_to_non_nullable
+as String?,gunPartId: freezed == gunPartId ? _self.gunPartId : gunPartId // ignore: cast_nullable_to_non_nullable
 as String?,
   ));
 }
--- lib/src/features/lessons/blocs/gun_fill_bloc/gun_fill_state.dart
+++ lib/src/features/lessons/blocs/gun_fill_bloc/gun_fill_state.dart
@@ -16,9 +16,16 @@ abstract class GunFillState with _$GunFillState {
 @freezed
 abstract class GunPart with _$GunPart {
   const factory GunPart({
+    /// Id of the part, also used as the color code, for example #ff0000.
     required String id,
+
+    /// Path of the part in svg image
     required String path,
+
+    /// Fill color of the part in svg image.
     String? color,
+
+    /// Tracks whether the part has been filled.
     @Default(false) bool isFilled,
     Item? item,
   }) = _GunPart;
@@ -26,5 +33,14 @@ abstract class GunPart with _$GunPart {
 
 @freezed
 abstract class GunLabel with _$GunLabel {
-  const factory GunLabel({required String path, String? color}) = _GunLabel;
+  const factory GunLabel({
+    /// Path of the label in svg image
+    required String path,
+
+    /// Fill color of the label in svg image.
+    String? color,
+
+    /// Gun part id/color code that this label accepts when dropped on.
+    String? gunPartId,
+  }) = _GunLabel;
 }
