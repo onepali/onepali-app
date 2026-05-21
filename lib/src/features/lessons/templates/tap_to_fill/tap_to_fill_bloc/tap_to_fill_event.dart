866cf250eccb559bdf16b9a158e874aea1292854
Saroj Basnet <saroj.basnet1237@gmail.com>
2026-05-21 14:36:56 +0545

    refactor: enhance audio handling and state management in TapToFill feature, adding support for audioBeforeOptions state and updating background images

--- lib/src/features/lessons/templates/tap_to_fill/tap_to_fill_bloc/tap_to_fill_event.dart
+++ lib/src/features/lessons/templates/tap_to_fill/tap_to_fill_bloc/tap_to_fill_event.dart
@@ -5,6 +5,8 @@ class TapToFillEvent with _$TapToFillEvent {
   const factory TapToFillEvent.started(TapToFillLessonContent content) =
       _Started;
 
+  const factory TapToFillEvent.audioBeforeOptionsCompleted() =
+      _AudioBeforeOptionsCompleted;
   const factory TapToFillEvent.audioCompleted() = _AudioCompleted;
   const factory TapToFillEvent.optionTapped(Option option) = _OptionTapped;
 }
