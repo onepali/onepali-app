c4a213bd64aa9cc00d11fab4b7f45513756fa945
Saroj Basnet <saroj.basnet1237@gmail.com>
2026-05-26 13:36:32 +0545

    refactor: restructure achievement screen by introducing AchievementLayout for better organization, replace AchievementTabCard with AchievementCard, and enhance UI with animated counters and a new yellow banner

--- lib/src/core/model/achievement/achievement_model.dart
+++ lib/src/core/model/achievement/achievement_model.dart
@@ -29,30 +29,3 @@ class AchievementModel {
     );
   }
 }
-
-List<AchievementModel> achievementList = [
-  AchievementModel(
-    id: "1",
-    title: "Practice Hero Trophy",
-    subtitle: "days streak",
-    value: "0",
-    imageUrl: Assets.trophyAv,
-    color: AppColors.kPureSkyBlue,
-  ),
-  AchievementModel(
-    id: "2",
-    title: "Learning Champion Medal",
-    subtitle: "Completed activities",
-    value: "0",
-    imageUrl: Assets.medalAv,
-    color: AppColors.kButtonRed,
-  ),
-  AchievementModel(
-    id: "3",
-    title: "Star Collector Badge",
-    subtitle: "Total stars collected",
-    value: "0",
-    imageUrl: Assets.starAv,
-    color: AppColors.kButtonGreen,
-  ),
-];
